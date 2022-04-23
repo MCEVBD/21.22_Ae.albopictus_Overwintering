ba3a29659b3a12476ec919cdcfacb36717870939
BartholomayLab <ksusong@wisc.edu>
2022-04-23 15:18:03 -0500

    Function created to add variable to HOBO temperature files. WORKING

---
+++ 00_Scripts/01_FUNCTIONS.R
@@ -0,0 +1,12 @@
+## 01 FUNCTIONS
+##================##
+
+
+
+# function adds the location/tire specific varaibles to the the  raw HOBO temperarture files 
+temp.variable.add<- function(data, tireloc, tirenum, tirea, total){
+  data$location <- rep(tireloc,total)
+  data$number <- rep (tirenum, total)
+  data$ABC <- rep(tirea, total)
+  return(data)
+}
