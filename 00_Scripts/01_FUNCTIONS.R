7df53d91a9e3701e3c2e38c6f03e86e1709545c4
BartholomayLab <ksusong@wisc.edu>
2022-05-03 07:33:06 -0500

    Differnt approch taken, no longer used

--- 00_Scripts/01_FUNCTIONS.R
+++ 00_Scripts/01_FUNCTIONS.R
@@ -11,12 +11,3 @@ temp.variable.add<- function(data, tireloc, tirenum, tirea, total){
   return(data)
 }
 
-
-# Function corrects date and time str adn makes a single DateTime col.
-edit.date <- function(df){
-  df$date <- mdy(df$date)   #correct date format
-  df$time <- hm(df$time)    #correct time format 
-  df$DateTime <- mdy_hm(paste(df$date, df$time)) # create Datetime col.
-  return(df)
-}
-  
