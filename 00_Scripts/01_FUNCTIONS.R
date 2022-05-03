d2feaa6266de468e15806f7743cc10e541ee3e63
BartholomayLab <ksusong@wisc.edu>
2022-05-03 07:21:21 -0500

    new function added. UNTESTED

--- 00_Scripts/01_FUNCTIONS.R
+++ 00_Scripts/01_FUNCTIONS.R
@@ -10,3 +10,13 @@ temp.variable.add<- function(data, tireloc, tirenum, tirea, total){
   data$ABC <- rep(tirea, total)
   return(data)
 }
+
+
+# Function corrects date and time str adn makes a single DateTime col.
+edit.date <- function(df){
+  df$date <- mdy(df$date)   #correct date format
+  df$time <- hm(df$time)    #correct time format 
+  df$DateTime <- mdy_hm(paste(df$date, df$time)) # create Datetime col.
+  return(df)
+}
+  
