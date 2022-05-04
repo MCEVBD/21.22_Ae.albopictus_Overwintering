4b61265b1a2d71c45d2a0e3d8e316cc6ccf162a8
BartholomayLab <ksusong@wisc.edu>
2022-05-04 14:16:36 -0500

    Corrected for 21.22 project. annotations added for clarity

--- 00_Scripts/SNODAS_scripts/04_depthsitesSNODAS.R
+++ 00_Scripts/SNODAS_scripts/04_depthsitesSNODAS.R
@@ -14,25 +14,27 @@ shhh(library(tidyverse))
 shhh(library(lubridate))
 shhh(library(ncdf4))
 
-# list of 2018 SNODAS files
-file.list <- list.files(path = "/Volumes/Seagate/SNODAS/studydates",pattern=".nc", full.names=TRUE)
+# list SNODAS files
+file.list <- list.files(path = "/Volumes/Seagate/SNODAS/snowdepth",pattern=".nc", full.names=TRUE)
 # vector of dates
-startdate <- ymd("2018-11-01")
-enddate <- ymd("2018-12-31")
+startdate <- ymd("2021-10-01")
+enddate <- ymd("2022-04-16")
 dates2018 <- seq(startdate, enddate, by = "days")
 # site locations 
-sites <- shapefile("~/Documents/CBS_PhD/albopictus_OW/Data/site_points/site_points1.shp")
+sites <- shapefile("00_Data/Shapefiles/Study_locations/study_location.shp")
 
 
 # extraxt the site snow depth 
 
-data <- data.frame(site = NA, date = ymd( NA), snow_depth = NA)
+data <- data.frame(site = NA, date = ymd( NA), snow_depth = NA)  # empty df
 for (i in 1:length(file.list)){
-  snowdas_data <- raster::raster(file.list[i])
-  temp <- data.frame(site = sites@data$Number, date = ymd(rep(dates2018[i], 18)), snow_depth = rep(NA, 18))
-  temp[,3] <- raster::extract(snowdas_data, sites)
-  data <- rbind(data,temp)
+  snowdas_data <- raster::raster(file.list[i])                   # select the i file in the list 
+  temp <- data.frame(site = sites@data$Site_Name,                # fill site with site_names ( Site_Names is defined in shape files)
+                     date = ymd(rep(dates2018[i], 6)),           # date repeated (rep = # of sites)
+                     snow_depth = rep(NA, 6))                    # blank NAs to fill with snowdepth
+  temp[,3] <- raster::extract(snowdas_data, sites)               # extract snow depth 
+  data <- rbind(data,temp)                                       # bind rows togauther 
 }
 
-#write file 
-write_csv(data,'~/Documents/CBS_PhD/albopictus_OW/Data/OWsitesnow.csv')
+# write file 
+# write_csv(data,'~/Documents/CBS_PhD/Ae.albo_OW_2021/21.22_Ae.albopictus_Overwintering/00_Data/21.22_SNODAS.csv')
