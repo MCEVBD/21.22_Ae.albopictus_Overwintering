587bea35e6e39dbd01abd3d24a2a073023eda18f
BartholomayLab <ksusong@wisc.edu>
2022-05-05 16:27:38 -0500

    Script 03 started

---
+++ 00_Scripts/03_inital_survvival_data_proc.R
@@ -0,0 +1,37 @@
+#' ---
+#' title: "03 Inital Survial Visulization (Pre temperature analysis)"
+#' author: "Katie M. Susong"
+#' date: "05-May-2022"
+#' --- 
+
+#' Initial formating and visulization of surival data
+#' ==================================================
+#' 
+#'  New varaibles generated:
+#'     - Tire_num
+#'     - ABC (orintations)
+#'     - number
+#'     - per.sur
+#'     - rel.per.sur
+#'     - per.red
+#'     
+#' visulizations : 
+#'      -
+#'      -
+#'      -
+
+
+#libraries
+library(readr)
+
+######### Import data #######
+
+data <- read_csv("00_Data/21.22_section_survival_data.csv",
+                 col_types = cols(dead.hatch1 = col_integer(),dead.hatch2 = col_integer(), dead.hatch3 = col_integer(), dead.hatch4 = col_integer(),
+                                  eeg1b = col_integer(), egg1 = col_integer(),
+                                  live.hatch1 = col_integer(), live.hatch2 = col_integer(), live.hatch3 = col_integer(), live.hatch4 = col_integer(),
+                                  sheet = col_factor(levels = c("A", "B", "C")), 
+                                  site = col_factor(levels = c("Spo","Han", "Arl", "Dek", "Bon", "Car")), 
+                                  x = col_number(), y = col_number()))
+
+
