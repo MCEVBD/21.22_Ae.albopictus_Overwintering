d90f793a4276ba404a3fa22d6e3902370b41ac62
BartholomayLab <ksusong@wisc.edu>
2022-05-27 11:04:56 -0500

    formating (21/22 data) done.

--- 00_Scripts/10_18.19glm_21.22predict.R
+++ 00_Scripts/10_18.19glm_21.22predict.R
@@ -12,9 +12,15 @@
 #' To do this the 21/22 data needs a few format adjustment to match 18/19 data. 
 #' The format changes are lsited below:
 #'    1. Factor of SNOWDAS created  
+#'    2. remane MeanT_Air to MeanT_Ambient
+#'    3. rename location to site (NOTE: in 18/19 study 3 tires where at 3 sites per location
+#'    where as in 21/22 all tires were located at a sigle site at each location. As such "site" 
+#'    from 18/19 is best reflected by "location" in 21/22)
 
 # Libraries #
 library(lubridate)
+library(dplyr)
+library(lme4)
 
 #### Import data ####
 #18/19 data
@@ -37,7 +43,10 @@ data          <- filter (data, Date < "2022-04-14" )
 
 # add catagorical snow values @ 100mm bins
 data$snow_FAC10 <- cut(data$snodas, breaks = c(-1, 0,100,200,300,400), labels = c("none", ">100", ">200", ">300", ">400"))
-
+# rename MeanT_Air
+data <- rename(data, MeanT_Ambient = "MeanT_Air")
+# rename location
+data <- rename(data, site = "location")
 
 ##### Run 18/19 model ####
 mixed.model2 <- lmer(MeanT_Tire ~ MeanT_Ambient + relevel(snow_FAC10, ref = 5)+ (1|site), data = DIA)
