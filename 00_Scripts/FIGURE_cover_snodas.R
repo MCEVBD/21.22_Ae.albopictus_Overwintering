767669279c9dad96f7c93c098696ba0a29035085
BartholomayLab <ksusong@wisc.edu>
2022-07-19 14:21:14 -0500

    new figure created. proportions of cover in each depth

--- 00_Scripts/FIGURE_cover_snodas.R
+++ 00_Scripts/FIGURE_cover_snodas.R
@@ -47,7 +47,7 @@ p1 <- ggplot(data, aes(snow_FAC10,MeanT_Diff)) +
 
 p2 <- ggplot(data, aes(snow_FAC10, cover, fill = cover))+
   geom_bar(stat = "identity", width = 0.5) + 
-  scale_fill_manual(values = c("#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#084594"), 
+  scale_fill_manual(values = c("#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#084594"), # blue color scale
                     na.value="black", 
                     labels = c("Uncovered Tire", "Only Side Covered", "Only Top Covered","Side and Top Covered")) +
   theme_classic()+
@@ -60,7 +60,7 @@ p2 <- ggplot(data, aes(snow_FAC10, cover, fill = cover))+
   theme(legend.title = element_blank(), 
         legend.position = "top")
 
-#'### Plot Cover forest plot 
+#'### Plot 3: Cover forest plot 
 #'
 #'
 
@@ -95,6 +95,61 @@ p3 <- est%>%
 
 p3
 
+#'### PLOT 4: Cover and Snow depth 
+#'
+#'
+
+# group by snow depth (factor) and cover to result in  a tally of of cover at each depth
+cover.tally <-data %>% 
+  group_by(snow_FAC10,cover) %>%
+  tally()
+
+# total number of measurements taken at each depth. 
+snow.tally <- data %>% 
+  group_by(snow_FAC10) %>%
+  tally()
+
+#check that the same total number is generated
+sum(snow.tally$n)
+sum(cover.tally$n)
+
+#create empty column for the proportion of each cover type at each snow depth 
+cover.tally$prop <- NA
+# add the proportion value by dividing the tally at each cover by the total a the same snow cover
+## none
+cover.tally$prop[cover.tally$snow_FAC10 == "none"] <-
+  cover.tally$n[cover.tally$snow_FAC10 == "none"] / snow.tally$n[snow.tally$snow_FAC10 == "none"]
+## >100
+cover.tally$prop[cover.tally$snow_FAC10 == ">100"] <-
+  cover.tally$n[cover.tally$snow_FAC10 == ">100"] / snow.tally$n[snow.tally$snow_FAC10 == ">100"]
+## >200
+cover.tally$prop[cover.tally$snow_FAC10 == ">200"] <-
+  cover.tally$n[cover.tally$snow_FAC10 == ">200"] / snow.tally$n[snow.tally$snow_FAC10 == ">200"]
+## >300
+cover.tally$prop[cover.tally$snow_FAC10 == ">300"] <-
+  cover.tally$n[cover.tally$snow_FAC10 == ">300"] / snow.tally$n[snow.tally$snow_FAC10 == ">300"]
+## >400
+cover.tally$prop[cover.tally$snow_FAC10 == ">400"] <-
+  cover.tally$n[cover.tally$snow_FAC10 == ">400"] / snow.tally$n[snow.tally$snow_FAC10 == ">400"]
+
+#'#### Create Plot
+#'
+#'
+p4 <- ggplot(cover.tally, aes(snow_FAC10, prop, fill = cover))+
+  geom_bar(stat = "identity", width = 0.5) +
+  scale_fill_manual(values = c("#9ECAE1", "#6BAED6", "#4292C6", "#2171B5", "#084594"), # blue color scale
+                    na.value="black", 
+                    labels = c("Uncovered Tire", "Only Side Covered", "Only Top Covered","Side and Top Covered")) +
+  theme_classic()+
+  xlab("Snow Depth, categorial (mm), SNODAS") +
+  scale_x_discrete(labels = c("No Snow", "less than 100 mm ", "100 to 199 mm", 
+                              "200 to 299 mm", "more than 300 mm")) +
+  ylab("Proportion of Observations") +
+  theme(axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 40, hjust= 1)) +
+  theme(legend.title = element_blank(), 
+        legend.position = "top")
+
 #'### Make merged plot option
 #'
 
@@ -115,6 +170,7 @@ setwd("~/Documents/CBS_PhD/Ae.albo_OW_2021/03_figures_presentation/Figures")
 ggsave("cover_violin.pdf", plot = p1, width = 5, height = 5, units = "in", dpi = 500)
 ggsave("cover_bar.pdf",    plot = p2, width = 7, height = 5, units = "in", dpi = 500)
 ggsave("cover_model.pdf",  plot = p3, width = 5, height = 3, units = "in", dpi = 500)
+ggsave("cover_bar_prop.pdf",  plot = p4, width = 5, height = 3, units = "in", dpi = 500)
 
 
   
